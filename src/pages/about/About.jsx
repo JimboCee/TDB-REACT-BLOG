fe6518964da1d7095e553c0b8156d96fbc8f4ef1
JimboCee <49816802+JimboCee@users.noreply.github.com>
2022-04-27 12:47:31 +0100

    about page added

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -6,8 +6,8 @@ export default function Home() {
     <div className="About">
         <Header/>
         <div className="aboutText">
-            <h1>About The Developer Academy</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            <h1 className="aboutH1">About The Developer Academy</h1>
+            <p className="aboutP">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
             Fuga dolorem repellat est eius commodi fugit magnam id possimus doloribus atque pariatur, deleniti dolores quo mollitia alias,
             sequi minus architecto delectus?
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nulla nihil sunt maxime adipisci dolorum saepe tenetur id ducimus 
@@ -17,7 +17,7 @@ export default function Home() {
             molestiae similique assumenda nam cupiditate eos, rem eum totam corrupti fugiat, esse dolores molestias. 
             Commodi, quis eos.</p>
             <br></br><br></br>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            <p className="aboutP">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
             Fuga dolorem repellat est eius commodi fugit magnam id possimus doloribus atque pariatur, deleniti dolores quo mollitia alias,
             sequi minus architecto delectus?
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nulla nihil sunt maxime adipisci dolorum saepe tenetur id ducimus 
